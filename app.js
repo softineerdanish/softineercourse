41671be8e25aca122d962541f5d8a6f77fdf9bc9
danishkhan830 <59353584+danishkhan830@users.noreply.github.com>
2024-02-05 13:40:30 +0530

    cors added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import {config} from "dotenv";
 import ErrorMddleware from "./middlewares/Error.js"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 config({
@@ -19,6 +20,11 @@ app.use(express.urlencoded({
 //cokieParser
 app.use(cookieParser());
 
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"]
+}))
 
 
 //Importing And using Routes
@@ -38,6 +44,11 @@ app.use("/api/v1", other)
 
 
 export default app;
+
+app.get("/", (req,res)=>{
+    res.send(`<h1>Site is Working . CLick <a
+    href=${process.env.FRONTEND_URL}>here</a> to visit Frontend </h1>`)
+})
 
 
 
