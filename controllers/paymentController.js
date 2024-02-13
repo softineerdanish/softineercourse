326cdb7e4a04ca1896994e6365b26b49bf00ad47
softineerdanish <108171025+softineerdanish@users.noreply.github.com>
2024-02-13 17:38:54 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -93,7 +93,7 @@ export const cancelSubscription = catchAsyncError(async(req,res,next)=>{
   const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
 
 if(refundTime > gap){
-  await instance.payments.refunds(payment.razorpay_payment_id);
+  await instance.payments.refund(payment.razorpay_payment_id);
   refund = true;
 }
 
